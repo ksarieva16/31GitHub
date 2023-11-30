97d40c63509649a6c3bfba6fbda3b89158e8f451
Karlygach <sarieva16@gmail.com>
2023-11-30 22:21:30 +0600

    Day2

---
+++ src/main/java/Day2.java
@@ -0,0 +1,11 @@
+public class Day2 {
+    public static void main(String args[]) {
+        //write your code here
+        String inviter = "Steve Jobs";
+        String invitee = "WeDevX Student";
+        String topic = "Apple Pay Planning";
+
+        String invitationEmail = "Dear WeDevX Student,\nSteve Jobs is inviting you to Apple Pay Planning meeting.";
+        System.out.println(invitationEmail);
+    }
+}
